9957110064ae0928c6a2cf6bb2d95393dd352c33
nasitra <nasitra@users.noreply.github.com>
2015-04-20 23:17:42 +0900

    Add node

--- cmap.js
+++ cmap.js
@@ -2,6 +2,25 @@
   'use strict';
   var cmap = {};
 
+  var prop = function(initialValue) {
+    var cache = initialValue;
+    return function(value) {
+      if (typeof value === 'undefined')
+        return cache;
+      cache = value;
+    };
+  };
+
+  var Node = function(option) {
+    this.label = prop(option.label || '');
+    this.x = prop(option.x || 0);
+    this.y = prop(option.y || 0);
+    this.width = prop(option.width || 75);
+    this.height = prop(option.height || 30);
+  };
+
+  cmap.Node = Node;
+
   if (typeof module !== 'undefined' && module.exports)
     module.exports = cmap;
   else
--- test/cmap-test.coffee
+++ test/cmap-test.coffee
@@ -1,2 +1,18 @@
 assert = require('assert')
 cmap = require('../cmap.js')
+
+describe 'cmap', ->
+  it 'Node', ->
+    node = new cmap.Node
+      label: 'node'
+      x: 100
+      y: 200
+      width: 120
+      height: 45
+    assert.equal(node.label(), 'node');
+    assert.equal(node.x(), 100);
+    assert.equal(node.y(), 200);
+    assert.equal(node.width(), 120);
+    assert.equal(node.height(), 45);
+    node.x(200);
+    assert.equal(node.x(), 200);
